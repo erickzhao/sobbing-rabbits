79050d52aab79bcc3b8f908e8b1bdfb4a5cf610c
Andrei <andrei.ungur@mail.mcgill.ca>
2019-03-16 16:27:32 -0400

    Add cors stuff again...

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from sqlalchemy import create_engine
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from utils import app, db, encode_auth_token, decode_auth_token
 from purchases import get_purchases
 from messages import send_sms
@@ -13,11 +13,13 @@ def index():
     return "Hello world!"
 
 @app.route('/purchased_cars/<jwt>')
+@cross_origin()
 def get_purchased_cars(jwt):
      user_id = decode_auth_token(jwt)
      return jsonify(get_purchases(user_id))
 
 @app.route("/messages", methods=["POST"])
+@cross_origin()
 def send_sms_to_user():
     notifications = request.json["notifications"]
     body = ""
@@ -26,6 +28,7 @@ def send_sms_to_user():
     return jsonify(send_sms("number", body))
 
 @app.route('/login/<user>')
+@cross_origin()
 def login(user):
      return jsonify({"auth": encode_auth_token(user).decode('utf8')})
 
