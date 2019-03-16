03836e1a661a45558792c38ac19acc342fbdac55
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-16 16:37:39 -0400

    f: improve data info

---
+++ frontend/src/config/user1.js
@@ -0,0 +1,34 @@
+const user = {
+  air_tax: null,
+  brake_desc:
+    "Its proving grounds were the streets, autobahns and mountain switchbacks of Europe, and now the Honda CBF600SA is earning the same solid reputation right here in Canada. Whether you're an experienced rider or newer to motorcycling and ready to move up from a smaller bike, you'll love the CBF600SA's 599 cc DOHC inline-four engine with its smooth, responsive powerband. The engine is tuned to deliver extra punch at low rpm levels \u2014 exactly the kind of tractable, right-there power you want for busy city streets and curving country roads. A low seat height is adjustable up or down for added comfort, and a sculpted half-fairing with adjustable windscreen flows air around you to keep you comfortable even when rain clouds roll in and the temperature drops. And when the rain does start to fall, you'll really appreciate Honda's Combined Braking System with ABS, which provides remarkable control even on less-than-ideal road surfaces.",
+  car_id: 4566547,
+  car_photo:
+    'https://img.sm360.ca/images/newcar/2011/Honda/CBF600/SA/Sport/exteriorColors/2011_Honda_CBF600SA_Sport_003.jpg',
+  car_year: 2010,
+  date_sold: 'Tue, 15 Apr 2014 00:00:00 GMT',
+  drive_train: 'N/A',
+  engine_desc:
+    '<b>Engine type :</b> Liquid-cooled inline-four<br/><br/><b>Displacement :</b> 599 cc<br/><br/><b>Bore & stroke :</b> 67 mm x 42.5 mm<br/><br/><b>Compression ratio :</b> 11.6:1<br/><br/><b>Valve train :</b> Chain-driven DOHC, 4 valves per cylinder<br/><br/><b>Fuel delivery :</b> PGM-FI electronic fuel injection<br/><br/>',
+  engine_type: '4',
+  fuel: 'Gasoline',
+  fuel_highway: 4.5,
+  fuel_town: 5.4,
+  general_desc:
+    "Its proving grounds were the streets, autobahns and mountain switchbacks of Europe, and now the Honda CBF600SA is earning the same solid reputation right here in Canada. Whether you're an experienced rider or newer to motorcycling and ready to move up from a smaller bike, you'll love the CBF600SA's 599 cc DOHC inline-four engine with its smooth, responsive powerband. The engine is tuned to deliver extra punch at low rpm levels \u2014 exactly the kind of tractable, right-there power you want for busy city streets and curving country roads. A low seat height is adjustable up or down for added comfort, and a sculpted half-fairing with adjustable windscreen flows air around you to keep you comfortable even when rain clouds roll in and the temperature drops. And when the rain does start to fall, you'll really appreciate Honda's Combined Braking System with ABS, which provides remarkable control even on less-than-ideal road surfaces.",
+  green_tax: null,
+  induction: null,
+  inventory_make: 'Honda',
+  inventory_model: 'CBF600',
+  inventory_trim: null,
+  suspension_desc:
+    '<b>Front suspension :</b> 41 mm fork with spring preload adjustment; 108 mm (4.3 in.) travel<br/><br/><b>Rear suspension :</b> Single shock with spring preload adjustment; 125 mm (4.9 in.) travel<br/><br/>',
+  transmission_gear: 6,
+  user_id: 329825,
+  user_name: 'Arman Izadi',
+  warranty_class: null,
+  warranty_date: null,
+  warranty_km: null
+};
+
+export default user;
---
+++ frontend/src/config/user2.js
@@ -0,0 +1,30 @@
+const data = {
+  car_id: 4108071,
+  car_year: 2011,
+  date_sold: 'Fri, 04 May 2012 00:00:00 GMT',
+  drive_train: 'Front Wheel Drive',
+  engine_type: '8',
+  fuel: 'Unleaded',
+  induction: null,
+  inventory_make: 'Ford',
+  inventory_model: 'Mustang',
+  inventory_trim: 'GT',
+  transmission_gear: 0,
+  user_id: 323950,
+  user_name: 'John Smith',
+  warranty_class: 'Class A : 6 months or 10,000 km.',
+  air_tax: 100.0,
+  brake_desc:
+    '<b>Front Brake Type :</b> 16" Ventilated Disc<br/><b>Front Brake Diameter (mm/in) :</b> 294 mm / 11.6 in.<br/><b>Rear Brake Type :</b> 14" Solid Disc<br/><b>Rear Brake Diameter (mm/in) :</b> 262 mm / 10.3 in<br/><b>Anti-lock Braking System :</b> 4-sensor 4-channel with EBD<br/><b>Swept Area - front (sq. in) :</b> 229.7 sq. in.<br/><b>Swept Area - rear (sq. in) :</b> 152.0 sq. in.<br/><b>Total Swept Area (sq. in) :</b> 381.7 sq. in.',
+  engine_desc:
+    '<b>Engine Type :</b> 2.4L MIVEC<br/><b>Engine Code :</b> 4G69<br/><b>Valve Train :</b> SOHC 16 valve<br/><b>Displacement (cc/cu in.) :</b> 2378 cc / 145.1 cu. in.<br/><b>Bore x Stroke (mm/in.) :</b> 87.0 x 100.0 mm / 3.43 x 3.94 in.<br/><b>Compression ratio :</b> 9.5 : 1<br/><b>Horsepower @ RPM :</b> 162 hp @ 6,000 rpm<br/><b>Torque @ RPM (lbs-ft.) :</b> 162 lb.-ft. @ 4,000 rpm<br/><b>Engine Block :</b> Cast Iron<br/><b>Cylinder Heads :</b> Aluminum<br/><b>Redline RPM :</b> 6,500 rpm<br/><b>Fuel System :</b> MPI<br/><b>Recommended Fuel :</b> Unleaded (Regular)<br/><b>Radiator Core Size :</b> 747x350x16 mm<br/><b>Engine Oil Type :</b> 5W-20<br/><b>Engine Oil Capacity (L/qt.) :</b> 3.0-4.0 L / 3.2-4.2 qt.',
+  fuel_highway: 7.3,
+  fuel_town: 10.6,
+  general_desc:
+    'Athletic Performance\n\nThe aggressively sculpted style of the Eclipse Coupe is pedestal worthy. But from the moment you slip behind the wheel, the powerful rush of the engine combined with the ultra-smooth precision handling, leaves no doubt this muscular vehicle is best exercised on pavement.\n\nWhether you hit the highway or cruise the city, the Eclipse delivers the power you crave for your next adventure. \n\nChoose between two strong engines: the spirited GS 2.4-litre 4-cylinder engine or the explosive power of the GT\u2019s 3.8-litre V6 engine. \n\nThanks to its rigid frame and low mount multi-link rear suspension, the Eclipse delivers thrilling handling that keeps the rubber on the road.\n',
+  green_tax: null,
+  suspension_desc:
+    '<b>Front :</b> MacPherson strut<br/><b>Front Stabilizer Bar Diameter (mm) :</b> 22 mm<br/><b>Rear :</b> Low mount multi link<br/><b>Rear Stabilizer Bar Diameter (mm) :</b> 20 mm'
+};
+
+export default data;
