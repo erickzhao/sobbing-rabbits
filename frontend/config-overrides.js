81ab8aa6930627119c1ab5845ed0f572fec5a4a1
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-16 09:22:41 -0400

    Add create-react-app-antd

---
+++ frontend/config-overrides.js
@@ -0,0 +1,16 @@
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+
+module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {
+      "@green-6": "#00cc7a",
+      "@primary-color": "@green-6"
+    }
+  })
+);
