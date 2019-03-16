52220f38b6bcd9ac60267d1ce9d26c14f96d676c
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-16 10:45:41 -0400

    frontend: eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,51 @@
 import React from 'react';
-import { Form, Select, InputNumber, DatePicker, Switch, Slider, Button } from 'antd';
+import {
+  Form,
+  Select,
+  Input,
+  DatePicker,
+  Switch,
+  Slider,
+  Layout,
+  Card,
+  Row,
+  Col,
+  Avatar,
+  Button
+} from 'antd';
 import './App.css';
 
-const { Option } = Select;
+const { Header, Footer, Content } = Layout;
 
 const App = () => (
-  <Form style={{ marginTop: 32 }}>
-    <Form.Item label="数字输入框" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-      <InputNumber min={1} max={10} defaultValue={3} />
-      <span className="ant-form-text"> 台机器</span>
-      <a href="https://ant.design">链接文字</a>
-    </Form.Item>
-    <Form.Item label="开关" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-      <Switch defaultChecked />
-    </Form.Item>
-    <Form.Item label="滑动输入条" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-      <Slider defaultValue={70} />
-    </Form.Item>
-    <Form.Item label="选择器" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-      <Select defaultValue="lucy" style={{ width: 192 }}>
-        <Option value="jack">jack</Option>
-        <Option value="lucy">lucy</Option>
-        <Option value="disabled" disabled>
-          disabled
-        </Option>
-        <Option value="yiminghe">yiminghe</Option>
-      </Select>
-    </Form.Item>
-    <Form.Item label="日期选择框" labelCol={{ span: 8 }} wrapperCol={{ span: 8 }}>
-      <DatePicker />
-    </Form.Item>
-    <Form.Item wrapperCol={{ span: 8, offset: 8 }}>
-      <Button type="primary" htmlType="submit">
-        确定
-      </Button>
-      <Button style={{ marginLeft: 8 }}>取消</Button>
-    </Form.Item>
-  </Form>
+  <Layout>
+    <Header>My Car Dashboard</Header>
+    <Layout style={{ paddingTop: 100, minHeight: '100vh' }}>
+      <Content>
+        <Row justify="center" type="flex">
+          <Col>
+            <Card>
+              <Avatar size={64} icon="user" />
+              <Form style={{ marginTop: 32 }}>
+                <Form.Item>
+                  <Input placeholder="Login" />
+                </Form.Item>
+                <Form.Item>
+                  <Input placeholder="Password" />
+                </Form.Item>
+                <Form.Item style={{ textAlign: 'center' }}>
+                  <Button type="primary" htmlType="submit">
+                    Log in
+                  </Button>
+                </Form.Item>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+    <Footer>Hello</Footer>
+  </Layout>
 );
 
 export default App;
