fef8b1196f51c92c9533fbeaf9d9c332400be0a7
Erick Zhao <erick.zhao@mail.mcgill.ca>
2019-03-16 11:10:31 -0400

    f: use BEM for jank hacks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Layout } from 'antd';
 import './App.css';
+import Login from './components/Login';
 
 const { Header, Footer } = Layout;
 
 const App = () => (
   <Layout>
     <Header>My Car Dashboard</Header>
+    <Login />
     <Footer>Hello</Footer>
   </Layout>
 );
