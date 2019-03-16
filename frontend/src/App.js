95388ca5db9809f64e73c61316d4d7928cd6be0a
Suleman Malik <suleman.malik@mail.mcgill.ca>
2019-03-16 12:34:55 -0400

    adding appointment layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Layout } from 'antd';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import Appointment from './components/Appointment';
 
 const { Header, Footer } = Layout;
 
--- frontend/src/components/Appointment.jsx
+++ frontend/src/components/Appointment.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
-import { Layout } from 'antd';
+import {
+  DatePicker,
+  TimePicker,
+  Col,
+  Card,
+  Button,
+  Form,
+  Input,
+  Layout,
+  Row,
+  Typography,
+  Select,
+  Divider
+} from 'antd';
+import '../App.css';
+import locale from 'antd/lib/date-picker/locale/en_US';
 
-const Appointment = props => {
-  return <Layout>Appointment</Layout>;
+const { Content } = Layout;
+const Option = Select.Option;
+
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
+
+
+
+const appointment = props => {
+  return (
+    <Layout style={{ paddingTop: 100, minHeight: '100vh' }}>
+      <Content>
+        <Row type="flex" justify="center">
+          <Col>
+            <Card style={{display:"flex", width:350, justifyContent: "center"}}>
+              <div
+                style={{
+                  flexDirection: 'column',
+                  display: 'flex',
+                  height: 300,
+                  justifyContent: 'space-around'
+                }}
+              >
+                <Typography>Select an appointment type</Typography>
+                <Select defaultValue="Type 1" style={{ width: 120 }} onChange={handleChange}>
+                  <Option value="Type 2">Type 2</Option>
+                  <Option value="Type 3">Type 2</Option>
+                  <Option value="Type 4">Type 3</Option>
+                </Select>
+                <Typography>Select a date</Typography>
+                <DatePicker locale={locale} />
+                <Typography>Select a time</Typography>
+                <TimePicker />
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
 };
 
-export default Appointment;
+export default appointment;
