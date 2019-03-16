cb72a7ac2378119bf249f7f513d87b8b14924669
Andrei <andrei.ungur@mail.mcgill.ca>
2019-03-16 14:10:04 -0400

    Improve examples + improve endpoint

--- backend/purchases.py
+++ backend/purchases.py
@@ -4,7 +4,10 @@ headers = {'content-type': 'application/json'}
 base_url = 'https://hackathon-api.bdc.n360.io'
 
 def get_purchases(username):
-    user_id, user_name = users[username]
+    if username not in users:
+        return {"status": "User not found!"}
+
+    user_id, make, model, user_name = users[username]
     get_car_info = "select lead.contact_id, inventory_make, car_year, inventory_model, inventory_trim, induction, "\
                             "engine_type, drive_train, transmission_gear, fuel, warranty_class, warranty_date, warranty_km, "\
                             "date_sold, car_id "\
@@ -13,9 +16,27 @@ def get_purchases(username):
                             "inner join lead on lead.lead_car_info_id=lead_car_info.lead_car_info_id "\
                             "where lead.lead_status='Sold' "\
                             "and car_inventory.date_sold is not null "\
-                            f"and lead.contact_id={user_id};"
+                            f"and lead.contact_id={user_id} "\
+                            f"and car_inventory.inventory_make='{make}' "\
+                            f"and car_inventory.inventory_model='{model}';"
 
     result = db.engine.execute(get_car_info).first()
+    if not result:
+        return {"status": "No result found!"}
+
+    get_extra_info = "select engine_desc, suspension_desc, brake_desc, fuel_town, fuel_highway, general_desc, air_tax, green_tax "\
+                     "from car_lineup "\
+                     "inner join car_lineup_trim on car_lineup_trim.car_id = car_lineup.car_id "\
+                     f"where car_lineup.make = '{make}' "\
+                     f"and car_lineup.model = '{model}' "\
+                     "and engine_desc is not null "\
+                     "and suspension_desc is not null "\
+                     "and general_desc is not null;"
+
+    result_extra = db.engine.execute(get_extra_info).first()
+    if not result:
+        return {"status": "No cars found..."}
+
     user_data = {"user_id": result[0],
                  "user_name": user_name,
                  "inventory_make": result[1],
@@ -27,34 +48,18 @@ def get_purchases(username):
                  "drive_train": result[7],
                  "transmission_gear": result[8],
                  "fuel": result[9],
-                 "warrany_class": result[10],
+                 "warranty_class": result[10],
                  "warranty_date": result[11],
                  "warranty_km": result[12],
                  "date_sold": result[13],
-                 "car_id": result[14]
-                }  
+                 "car_id": result[14],
+                 "engine_desc": result_extra[0],
+                 "suspension_desc": result_extra[1],
+                 "brake_desc": result_extra[2],
+                 "fuel_town": result_extra[3],
+                 "fuel_highway": result_extra[4],
+                 "general_desc": result_extra[5],
+                 "air_tax": result_extra[6],
+                 "green_tax": result_extra[7]
+                }
     return user_data
-
-def get_extra_car_data(car_data):
-    make, model = car_data.split('-')
-    get_extra_info = "select engine_desc, suspension_desc, brake_desc, fuel_town, fuel_highway, general_desc, air_tax, green_tax "\
-                     "from car_lineup "\
-                     "inner join car_lineup_trim on car_lineup_trim.car_id = car_lineup.car_id "\
-                     f"where car_lineup.make = '{make}' "\
-                     f"and car_lineup.model = '{model}' "\
-                     "and engine_desc is not null "\
-                     "and suspension_desc is not null "\
-                     "and general_desc is not null;"
-    result = db.engine.execute(get_extra_info).first()
-    user_data = {
-        "engine_desc": result[0],
-        "suspension_desc": result[1],
-        "brake_desc": result[2],
-        "fuel_town": result[3],
-        "fuel_highway": result[4],
-        "general_desc": result[5],
-        "air_tax": result[6],
-        "green_tax": result[7]
-    }
-    return user_data
-
